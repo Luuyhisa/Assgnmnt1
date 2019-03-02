4b5baec9f6a33a73ee6509decd7f933d72f99185
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-03-02 21:56:07 +0200

    my commit (number 1)

--- src/test/java/ac/za/cput/Assgnmnt1/CollectionListSetMapTest.java
+++ src/test/java/ac/za/cput/Assgnmnt1/CollectionListSetMapTest.java
@@ -1,13 +1,9 @@
 package ac.za.cput.Assgnmnt1;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.lang.reflect.Array;
 import java.util.*;
-
 import static org.junit.Assert.*;
-
 public class CollectionListSetMapTest {
     CollectionListSetMap  CollectionListSetMap= new CollectionListSetMap();
     @Test
@@ -54,16 +50,3 @@ public class CollectionListSetMapTest {
         Assert.assertEquals(cardsList,exCol);
     }
 }
-
-/**
-String value = "{first_name = naresh,last_name = kumar,gender = male}";
-value = value.substring(1, value.length()-1);           //remove curly brackets
-        String[] keyValuePairs = value.split(",");              //split the string to creat key-value pairs
-        Map<String,String> map = new HashMap<>();
-
-        for(String pair : keyValuePairs)                        //iterate over the pairs
-        {
-        String[] entry = pair.split("=");                   //split the pairs to get key and value
-        map.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
-        }
-**/
