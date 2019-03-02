b309f9ac80d56527fb41aeec5d11437afcbbf895
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-03-02 18:39:34 +0200

    my 1st commit

---
+++ src/main/java/ac/za/cput/Assgnmnt1/CollectionListSetMap.java
@@ -0,0 +1,24 @@
+package ac.za.cput.Assgnmnt1;
+
+
+import java.util.HashMap;
+
+public class CollectionListSetMap {
+
+    //my 1st HashMap Method
+    public void hashMap(){
+        HashMap<String,Integer> mySiblings = new HashMap();
+        mySiblings.put("Lethu", 8);
+        mySiblings.put("Masintle", 28);
+        mySiblings.put("Thimna", 12);
+        mySiblings.put("Lungisa", 22);
+        System.out.print(mySiblings);
+    }
+    //My 1st ListClass
+    public void List(){
+
+
+
+    }
+}
+
